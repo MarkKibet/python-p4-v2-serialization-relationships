5e495aaff9b63d395113d0a6ed9319532fb5fb62
MarkKibet <kemboimarkbiwott@gmail.com>
2025-04-06 10:30:26 +0300

    worked on serialization

---
+++ models.py
@@ -0,0 +1,56 @@
+# server/models.py
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Zookeeper(db.Model, SerializerMixin):
+    __tablename__ = 'zookeepers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    birthday = db.Column(db.Date)
+
+    animals = db.relationship('Animal', back_populates='zookeeper')
+    serialize_rules = ('-animals.zookeeper',)
+
+
+class Enclosure(db.Model, SerializerMixin):
+    __tablename__ = 'enclosures'
+
+    id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', back_populates='enclosure')
+    serialize_rules = ('-animals.enclosure',)
+
+class Animal(db.Model, SerializerMixin):
+    __tablename__ = 'animals'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    species = db.Column(db.String)
+
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    enclosure = db.relationship('Enclosure', back_populates='animals')
+    zookeeper = db.relationship('Zookeeper', back_populates='animals')
+
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
+    def __repr__(self):
+        return f'<Animal {self.name}, a {self.species}>'
